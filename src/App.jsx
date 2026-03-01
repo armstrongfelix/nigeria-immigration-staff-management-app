f6224c3b67c5ce145631de72afa5a3af3ac53a47
Felix Armstrong Ezeudo <armstrongfelix4@gmail.com>
2026-03-01 15:06:24 +0100

    created data storage  file for the apllication
    inside the file added NIgeria states data, nigeria immigration ranks data, and staff status state data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
-      <Sidebar />
+      <Navbar />
     </div>
   );
 }
